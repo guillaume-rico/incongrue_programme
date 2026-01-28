2b511c92c538b8eb732dfc0524d25dd35ed96af3
Alliaume <alliaume.rico@gmail.com>
2026-01-28 15:19:30 +0100

    MAJ

--- incongrue_download_programme/incongrue_download_programme.py
+++ incongrue_download_programme/incongrue_download_programme.py
@@ -51,23 +51,20 @@ for numPage in range(1, 10):
         
             # print("Ligne : " + line)
             # Deux cas, soit c'est un évenement sur plusieurs jours soit sur un nombre d'heure
-            event = {}
-            event["date"] = ""
-            event["heure"] = ""
-            event["titre"] = ""
             if "tribe-event-time" in line :
                 # Evenement sur quelques heures 
                 cleanLine = line.replace('<span class="tribe-event-date-start">',"").replace('<span class="tribe-event-time">',"").replace('</span>',"").replace('</time>',"").strip()
                 cleanLineSplit = cleanLine.split(" ")
+                #print(cleanLineSplit)
                 
-                jourEvenement = datetime.datetime.strptime(cleanLineSplit[0] + " " + cleanLineSplit[1] + " " + cleanLineSplit[2],'%d %B %Y')
+                jourEvenement = datetime.datetime.strptime(cleanLineSplit[0] + " " + cleanLineSplit[1] + " " + str(anneeActuelle),'%d %m %Y')
                 jourTemp = jourEvenement.strftime('%A')
                 jour = jourTemp[0].upper() + jourTemp[1:]
                 event["dateHard"] = jourEvenement
                 event["date"] = jour + " " + jourEvenement.strftime('%d').lstrip("0")
                 
-                heureDepSplit = cleanLineSplit[4].split(":")
-                heureFinSplit = cleanLineSplit[6].split(":")
+                heureDepSplit = cleanLineSplit[3].split(":")
+                heureFinSplit = cleanLineSplit[5].split(":")
                 heuredepart = heureDepSplit[0]
                 minutedepart = heureDepSplit[1]
                 heurefin = heureFinSplit[0]
@@ -78,14 +75,33 @@ for numPage in range(1, 10):
                     minutefin = ""
                 
                 event["heure"] = heuredepart + "h" + minutedepart + "-" + heurefin + "h" + minutefin
-                event["titre"] = ""
             else :
                 # Evenement sur plusieurs jours
                 cleanLine = line.replace('<span class="tribe-event-date-start">',"").replace('<span class="tribe-event-date-end">',"").replace('</span>',"").replace('</time>',"").strip()
                 cleanLineSplit = cleanLine.split(" ")
-                jourEvenement = datetime.datetime.strptime(cleanLineSplit[0] + " " + cleanLineSplit[1] + " " + cleanLineSplit[2],'%d %B %Y')
+                #print("multijour - " + str(cleanLineSplit))
+                jourEvenement = datetime.datetime.strptime(cleanLineSplit[0] + " " + cleanLineSplit[1] + " " + str(anneeActuelle),'%d %m %Y')
                 event["dateHard"] = jourEvenement
-                event["date"] = "Du " + cleanLineSplit[0] + " au " + cleanLineSplit[4] 
+                event["date"] = "Du " + cleanLineSplit[0] + " au " + cleanLineSplit[3] 
+                
+            # On a deja eu le titre, on a maintenant la date, on peut écrire
+            if args.option_download == "monthevt" :
+                if event["dateHard"] < dateMax and event["dateHard"] >= dateMin :
+                    toWrite = 1
+                elif event["dateHard"] > dateMax :
+                    dateDepasse = 1
+                    
+            if args.option_download == "20evt" :
+                toWrite = 1
+            
+            if toWrite == 1 :
+                # On retire les événements récurants :
+                if "atelier couture" not in event["titre"].lower() and \
+                    "aide au numérique" not in event["titre"].lower() and \
+                    "course à pied" not in event["titre"].lower() :
+                    #print("Ecriture")
+                    f.write(event["date"] + ";" + event["heure"] + ";" + event["titre"] + ";\n")
+                    nbElement = nbElement + 1
                 
         if 'class="tribe-events-calendar-list__event-title-link tribe-common-anchor-thin"' in line :
             #print(line.replace('<span class="tribe-event-date-start">',""))
@@ -93,26 +109,16 @@ for numPage in range(1, 10):
         if nl >= 1 :
             nl = nl + 1
             if nl == 4 :
+                event = {}
+                event["date"] = ""
+                event["heure"] = ""
                 event["titre"] = html.unescape(line.replace('</a>',"").replace("\xa0"," ").strip())
                 #print(line.replace('</a>',"").strip())
-                # print(event)
+                #print(event)
                 toWrite = 0
-                if args.option_download == "monthevt" :
-                    if event["dateHard"] < dateMax and event["dateHard"] >= dateMin :
-                        toWrite = 1
-                    elif event["dateHard"] > dateMax :
-                        dateDepasse = 1
-                        
-                if args.option_download == "20evt" :
-                    toWrite = 1
-                
-                if toWrite == 1 :
-                    # On retire les événements récurants :
-                    if "atelier couture" not in event["titre"].lower() and \
-                        "aide au numérique" not in event["titre"].lower() and \
-                        "course à pied" not in event["titre"].lower() :
-                        f.write(event["date"] + ";" + event["heure"] + ";" + event["titre"] + ";\n")
-                        nbElement = nbElement + 1
+            elif nl < 6 :
+                print("nl : " + str(nl) + " - " + html.unescape(line.replace('</a>',"").replace("\xa0"," ").strip()))
+
     if args.option_download == "20evt" and nbElement >= 20 :
         break
     if dateDepasse == 1 :
